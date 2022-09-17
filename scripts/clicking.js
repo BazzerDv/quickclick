654dcc034edd6e11b2d9f1e3464e175dbd50f000
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-17 19:08:31 +0100

    Update clicking.js

--- scripts/clicking.js
+++ scripts/clicking.js
@@ -23,17 +23,10 @@ var Clicks = {
     addClick(){
     	this.setNbrOwned(this.nbrOwned + (1*this.mouseUpgrades))
     	this.clickLoop += 1
-    	if (this.clickLoop > (AnimationTest.length - 1)) {
+    	if (this.clickLoop > (Animation.length - 1)) {
     	    this.clickLoop = 0
     	}
-    	document.getElementById('animation').innerHTML=AnimationTest[this.clickLoop]
-    	//if ((this.clickLoop % 2)==0){
-    	//  document.getElementById('animation').innerHTML=Animation[0]
-    	//  this.clickLoop += 1
-       // } else {
-       //   document.getElementById('animation').innerHTML=Animation[1]
-       //   this.clickLoop += 1
-       // }
+    	document.getElementById('animation').innerHTML=Animation[this.clickLoop]
     },
     
     addMouseUpgrade(value, upgrade){
