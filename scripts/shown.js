1c450c30864e5ad6fe1ca4df156ff97002a3b593
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-17 14:19:50 +0100

    Fixes to reset

--- scripts/shown.js
+++ scripts/shown.js
@@ -1,8 +1,10 @@
 var Buttonconfig = {
-  buttonsreq: {mouseR: 10, clicker:50, trainedclicker: 200, mouseE: 300, spammer: 1000, vibrationengineer: 150000, collegedropout: 2000000},
-
+  buttonsAll: {mouseR: 10, clicker:50, trainedclicker: 200, mouseE: 300, spammer: 1000, vibrationengineer: 150000, collegedropout: 2000000},
+  buttonsAuto: {clicker:50, trainedclicker: 200, spammer:1000, vibrationengineer:150000, collegedropout:2000000},
+  buttonsManual: {mouseR:10, mouseE:300},
+  
   show(){
-    for (const [key, value] of Object.entries(this.buttonsreq)){
+    for (const [key, value] of Object.entries(this.buttonsAuto)){
       if (Clicks.nbrOwned >= value){
         document.getElementById(String(key)).style = 'visibility:visible'
       }
@@ -10,7 +12,7 @@ var Buttonconfig = {
   },
   
   hide(){
-    for (const [key, value] of Object.entries(this.buttonsreq)){
+    for (const [key, value] of Object.entries(this.buttonsAuto)){
       if (Clicks.nbrOwned >= value){
         document.getElementById(String(key)).style = 'visibility:hidden'
       }
@@ -18,7 +20,7 @@ var Buttonconfig = {
   },
   
   disable(){
-    for (const [key, value] of Object.entries(this.buttonsreq)){
+    for (const [key, value] of Object.entries(this.buttonsAll)){
       if (Clicks.nbrOwned < value){
         document.getElementById(String(key)).disabled = 'disabled'
       }
@@ -26,7 +28,7 @@ var Buttonconfig = {
   },
   
   enable(){
-    for (const [key, value] of Object.entries(this.buttonsreq)){
+    for (const [key, value] of Object.entries(this.buttonsAll)){
       if (Clicks.nbrOwned >= value){
         document.getElementById(String(key)).disabled = ''
       } else {
