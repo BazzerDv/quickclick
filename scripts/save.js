37baddef9fcb93707462284a902ae1f7aefe13df
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-19 22:08:57 +0100

    New autoclickers

--- scripts/save.js
+++ scripts/save.js
@@ -5,11 +5,13 @@ let Save = {
     save_data.clicksdata.nbrOwned = Clicks.nbrOwned;
     save_data.clicksdata.mouseUpgrades = Clicks.mouseUpgrades;
     save_data.autoclickersdata = {};
-    save_data.autoclickersdata.clickers = Autoclickers.nbrOwned.clickers.number;
-    save_data.autoclickersdata.trainedclickers = Autoclickers.nbrOwned.trainedclickers.number;
-    save_data.autoclickersdata.spammers = Autoclickers.nbrOwned.spammers.number;
-    save_data.autoclickersdata.vibrationengineers = Autoclickers.nbrOwned.vibrationengineers.number;
-    save_data.autoclickersdata.collegedropouts = Autoclickers.nbrOwned.collegedropouts.number;
+    for (const [key,value] of Object.entries(Autoclickers.nbrOwned)){
+      save_data.autoclickersdata.key = Autoclickers.nbrOwned[String(key)].number;
+    }
+    //save_data.autoclickersdata.trainedclickers = Autoclickers.nbrOwned.trainedclickers.number;
+    //save_data.autoclickersdata.spammers = Autoclickers.nbrOwned.spammers.number;
+    //save_data.autoclickersdata.vibrationengineers = Autoclickers.nbrOwned.vibrationengineers.number;
+    //save_data.autoclickersdata.collegedropouts = Autoclickers.nbrOwned.collegedropouts.number;
     this.save(save_data)
   },
   
