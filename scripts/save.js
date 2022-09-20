eb287cd26ce4e068e0d64821bd7725225c061f91
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-20 16:12:09 +0100

    Use of Loops to Shorten Code

--- scripts/save.js
+++ scripts/save.js
@@ -7,25 +7,30 @@ let Save = {
     save_data.autoclickersdata = {};
     for (const [key,value] of Object.entries(Autoclickers.nbrOwned)){
       save_data.autoclickersdata[String(key)] = {}
-    }
-    //for (const [key,value] of Object.entries(Autoclickers.nbrOwned)){
-    //  save_data.autoclickersdata.String(key) = Autoclickers.nbrOwned[String(key)].number;
-    //}
-    save_data.autoclickersdata.clickers.number = Autoclickers.nbrOwned.clickers.number;
-    save_data.autoclickersdata.trainedclickers.number = Autoclickers.nbrOwned.trainedclickers.number;
-    save_data.autoclickersdata.spammers.number = Autoclickers.nbrOwned.spammers.number;
-    save_data.autoclickersdata.tappers.number = Autoclickers.nbrOwned.tappers.number;
-    save_data.autoclickersdata.vibrationengineers.number = Autoclickers.nbrOwned.vibrationengineers.number;
-    save_data.autoclickersdata.collegedropouts.number = Autoclickers.nbrOwned.collegedropouts.number;
-    save_data.autoclickersdata.babies.number = Autoclickers.nbrOwned.babies.number;
+    };
     
-    save_data.autoclickersdata.clickers.showing = Autoclickers.nbrOwned.clickers.showing;
-    save_data.autoclickersdata.trainedclickers.showing = Autoclickers.nbrOwned.trainedclickers.showing;
-    save_data.autoclickersdata.spammers.showing = Autoclickers.nbrOwned.spammers.showing;
-    save_data.autoclickersdata.tappers.showing = Autoclickers.nbrOwned.tappers.showing;
-    save_data.autoclickersdata.vibrationengineers.showing = Autoclickers.nbrOwned.vibrationengineers.showing;
-    save_data.autoclickersdata.collegedropouts.showing = Autoclickers.nbrOwned.collegedropouts.showing;
-    save_data.autoclickersdata.babies.showing = Autoclickers.nbrOwned.babies.showing;
+    for (const [key,value] of Object.entries(Autoclickers.nbrOwned)){
+      save_data.autoclickersdata[String(key)].number = Autoclickers.nbrOwned[String(key)].number;
+    };
+    
+    for (const [key,value] of Object.entries(Autoclickers.nbrOwned)){
+      save_data.autoclickersdata[String(key)].showing = Autoclickers.nbrOwned[String(key)].showing;
+    };
+    //save_data.autoclickersdata.clickers.number = Autoclickers.nbrOwned.clickers.number;
+    //save_data.autoclickersdata.trainedclickers.number = Autoclickers.nbrOwned.trainedclickers.number;
+    //save_data.autoclickersdata.spammers.number = Autoclickers.nbrOwned.spammers.number;
+    //save_data.autoclickersdata.tappers.number = Autoclickers.nbrOwned.tappers.number;
+    //save_data.autoclickersdata.vibrationengineers.number = Autoclickers.nbrOwned.vibrationengineers.number;
+    //save_data.autoclickersdata.collegedropouts.number = Autoclickers.nbrOwned.collegedropouts.number;
+    //save_data.autoclickersdata.babies.number = Autoclickers.nbrOwned.babies.number;
+    
+    //save_data.autoclickersdata.clickers.showing = Autoclickers.nbrOwned.clickers.showing;
+    //save_data.autoclickersdata.trainedclickers.showing = Autoclickers.nbrOwned.trainedclickers.showing;
+    //save_data.autoclickersdata.spammers.showing = Autoclickers.nbrOwned.spammers.showing;
+    //save_data.autoclickersdata.tappers.showing = Autoclickers.nbrOwned.tappers.showing;
+    //save_data.autoclickersdata.vibrationengineers.showing = Autoclickers.nbrOwned.vibrationengineers.showing;
+    //save_data.autoclickersdata.collegedropouts.showing = Autoclickers.nbrOwned.collegedropouts.showing;
+    //save_data.autoclickersdata.babies.showing = Autoclickers.nbrOwned.babies.showing;
     this.save(save_data)
   },
   
