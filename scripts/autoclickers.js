d95a6786d7767b3703564393efbcae8a9c41bff9
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-21 22:06:18 +0100

    New autoclickers

--- scripts/autoclickers.js
+++ scripts/autoclickers.js
@@ -10,7 +10,10 @@ let Autoclickers = {
                'vibrationengineers': {cost: 100000, number:0, cps:500, buttonString: 'Vibration Engineer', showing: false, state: ''},
                'collegedropouts': {cost:1500000, number:0, cps:15000, buttonString: 'College Dropout', showing: false, state: ''},
 	       'babies': {cost:20000000, number:0, cps:300000, buttonString: 'Baby', showing: false, state: ''},
-	       'bossbabies': {cost:30000000, number:0, cps:2000000, buttonString: 'Boss Baby', showing: false, state: ''}},
+	       'bossbabies': {cost:30000000, number:0, cps:2000000, buttonString: 'Boss Baby', showing: false, state: ''},
+	       'unemployedengineers': {cost:500000000, number:0, cps:40000000, buttonString: 'Unemployed Engineer', showing: false, state: ''},
+	       'testers': {cost:10000000000, number:0, cps:2000000000, buttonString: 'Tester', showing: false, state: ''},
+	       'developers': {cost:3000000000000, number:0, cps:10000000000000, buttonString: 'Developer', showing: false, state: ''},},
     
     onload(){
       if (Load.found == true){
