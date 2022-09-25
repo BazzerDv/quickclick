2bc67374892ff21ac9bfb7afb66defae5546ecbc
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-25 21:08:33 +0100

    Update

--- scripts/reset.js
+++ scripts/reset.js
@@ -5,6 +5,7 @@ let reset = function(){
   Autoclickers.onload()
   Runtime.onload()
   document.getElementById('fastestrun').innerHTML = ''
+  document.getELementById('bonus').innerHTML = ''
   main.oldNbrOwned = 0
   Save.getSaveData()
 }
