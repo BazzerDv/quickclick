aa34a14b0b95a97f2cbbe8c18bbc2e7d9e854ef8
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-28 18:54:48 +0100

    Update autoclickers.js

--- scripts/autoclickers.js
+++ scripts/autoclickers.js
@@ -1,19 +1,19 @@
 let Autoclickers = {
-    Cps: 0,
-    Cpots: 0,
-    OldButtons: '',
-    Mspc: 0,  //Milliseconds per click
-    nbrOwned: {'clickers': {cost:50, number: 0, cps: 1, buttonString: 'Clicker', showing: false, state: ''},
-         'trainedclickers': {cost:200, number:0, cps:5, buttonString: 'Trained Clicker', showing: false, state: ''},
-         'spammers': {cost:1000, number:0, cps:30, buttonString: 'Spammer', showing: false, state: ''},
-	       'tappers': {cost:20000, number:0, cps:100, buttonString: 'Tapper', showing: false, state: ''},
-         'vibrationengineers': {cost: 100000, number:0, cps:500, buttonString: 'Vibration Engineer', showing: false, state: ''},
-         'collegedropouts': {cost:1500000, number:0, cps:15000, buttonString: 'College Dropout', showing: false, state: ''},
-	       'babies': {cost:20000000, number:0, cps:300000, buttonString: 'Baby', showing: false, state: ''},
-      	 'bossbabies': {cost:30000000, number:0, cps:2000000, buttonString: 'Boss Baby', showing: false, state: ''},
-       	 'unemployedengineers': {cost:500000000, number:0, cps:40000000, buttonString: 'Unemployed Engineer', showing: false, state: ''},
-      	 'testers': {cost:10000000000, number:0, cps:2000000000, buttonString: 'Tester', showing: false, state: ''},
-      	 'developers': {cost:3000000000000, number:0, cps:10000000000000, buttonString: 'Developer', showing: false, state: ''}},
+  Cps: 0,
+  Cpots: 0,
+  OldButtons: '',
+  Mspc: 0,  //Milliseconds per click
+  nbrOwned: {'clickers': {cost:50, number: 0, cps: 1, buttonString: 'Clicker', showing: false, state: ''},
+    'trainedclickers': {cost:200, number:0, cps:5, buttonString: 'Trained Clicker', showing: false, state: ''},
+    'spammers': {cost:1000, number:0, cps:30, buttonString: 'Spammer', showing: false, state: ''},
+    'tappers': {cost:20000, number:0, cps:100, buttonString: 'Tapper', showing: false, state: ''},
+    'vibrationengineers': {cost: 100000, number:0, cps:500, buttonString: 'Vibration Engineer', showing: false, state: ''},
+    'collegedropouts': {cost:1500000, number:0, cps:15000, buttonString: 'College Dropout', showing: false, state: ''},
+	  'babies': {cost:20000000, number:0, cps:300000, buttonString: 'Baby', showing: false, state: ''},
+    'bossbabies': {cost:30000000, number:0, cps:2000000, buttonString: 'Boss Baby', showing: false, state: ''},
+ 	  'unemployedengineers': {cost:500000000, number:0, cps:40000000, buttonString: 'Unemployed Engineer', showing: false, state: ''},
+		'testers': {cost:10000000000, number:0, cps:2000000000, buttonString: 'Tester', showing: false, state: ''},
+		'developers': {cost:3000000000000, number:0, cps:10000000000000, buttonString: 'Developer', showing: false, state: ''}},
     
     onload(){
       if (Load.found == true){
