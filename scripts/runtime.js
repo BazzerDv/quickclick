b641763085a872864bc8870c722cda22d649ef29
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-29 07:57:39 +0100

    Code Cleaning

--- scripts/runtime.js
+++ scripts/runtime.js
@@ -7,6 +7,7 @@ let Runtime = {
   runMinutes: 0,
   runSeconds: 0,
   runMilliseconds: 0,
+  timeDivisions: {'runDays':86400, runHours:3600, runMinutes:60, runSeconds:1},
   
   onload(){
     if ((Load.found == true) && ('runtimedata' in Load.data)){
@@ -24,10 +25,16 @@ let Runtime = {
   },
   
   updateRun(newvalue, run){
-    value = newvalue
+    let value = newvalue
     this[String(run)].time = value
     
-    if (this[String(run)].time > 86400){
+    for (const [key,number] in Object.entries(this.timeDivisions)){
+      if this[String(run)].time > number{
+        this[String(key)] = Math.floor(value / number)
+        value %= value
+      }
+    }
+    /*if (this[String(run)].time > 86400){
       this.runDays = Math.floor(value / 86400)
       value %= 86400
     }
@@ -45,9 +52,10 @@ let Runtime = {
     if (this[String(run)].time > 1){
       this.runSeconds = Math.floor(value / 1)
       value %= 1
-    }
+    }*/
     
     this.runMilliseconds = (Math.floor(value*10))
+    
     if (((this.fastestrun.time !==0) && (run == 'fastestrun')) || (run == 'currentrun')){
       document.getElementById(String(run)).innerHTML = this[String(run)].runString + this.runDays + ':' + this.runHours + ':' + this.runMinutes + ':' + this.runSeconds + '.' + this.runMilliseconds
     }
