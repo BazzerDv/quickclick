4acea285d2659f638a10bde30a72d8ccc1464a26
BazzerDv <113629148+BazzerDv@users.noreply.github.com>
2022-09-29 08:13:13 +0100

    Update clicking.js

--- scripts/clicking.js
+++ scripts/clicking.js
@@ -1,41 +1,43 @@
 var Clicks = {
-    nbrOwned : 0,
-    mouseUpgrades: 0,
-    clickLoop: 0,
-    onload(){
-      if (Load.found == true){
-    	this.setNbrOwned(Load.data.clicksdata.nbrOwned)
-    	this.mouseUpgrades = Load.data.clicksdata.mouseUpgrades
-      } else {
-        this.setNbrOwned(0)
+  nbrOwned : 0,
+	mouseUpgrades: 0,
+  clickLoop: 0,
+  onload(){
+		if (Load.found == true){
+   	this.setNbrOwned(Load.data.clicksdata.nbrOwned)
+  	this.mouseUpgrades = Load.data.clicksdata.mouseUpgrades
+		} else {
+      this.setNbrOwned(0)
     	this.mouseUpgrades = 1
-      }
-      document.getElementById('mouseupgrades').innerHTML = 'Mouse Rating: '+ (this.mouseUpgrades)
-      document.getElementById('animation').innerHTML=Animation[this.clickLoop]
-    },
-    
-    setNbrOwned(value){
-    	this.nbrOwned = Number(value)
-    	document.getElementById('clicks').innerHTML = 'Clicks: '+Math.floor(value)
-    },
-    
-    addClick(){
-    	this.setNbrOwned(this.nbrOwned + (1*this.mouseUpgrades))
-    	this.clickLoop += 1
-    	if (this.clickLoop > (Animation.length - 1)) {
-    	    this.clickLoop = 0
-    	}
-    	document.getElementById('animation').innerHTML=Animation[this.clickLoop]
-        main.update()
-    },
-    
-    addMouseUpgrade(value, upgrade){
-   	if (this.nbrOwned >= value){
-            this.mouseUpgrades += upgrade
-    	    document.getElementById('mouseupgrades').innerHTML = 'Mouse Rating: '+ (this.mouseUpgrades)
-    	    this.setNbrOwned(this.nbrOwned - value)
-    	    main.update()
-    	}
     }
+		document.getElementById('mouseupgrades').innerHTML = 'Mouse Rating: '+ (this.mouseUpgrades)
+    document.getElementById('animation').innerHTML=Animation[this.clickLoop]
+		document.getElementById('clickbutton').title = 'Click Me! (+' + (1*this.mouseUpgrades) + ')'
+	},
+    
+  setNbrOwned(value){
+   	this.nbrOwned = Number(value)
+		document.getElementById('clicks').innerHTML = 'Clicks: '+Math.floor(value)
+  },
+    
+  addClick(){
+   	this.setNbrOwned(this.nbrOwned + (1*this.mouseUpgrades))
+    this.clickLoop += 1
+    if (this.clickLoop > (Animation.length - 1)) {
+			this.clickLoop = 0
+  	}
+  	document.getElementById('animation').innerHTML=Animation[this.clickLoop]
+    main.update()
+  },
+    
+	addMouseUpgrade(value, upgrade){
+ 	  if (this.nbrOwned >= value){
+      this.mouseUpgrades += upgrade
+  	  document.getElementById('mouseupgrades').innerHTML = 'Mouse Rating: '+ (this.mouseUpgrades)
+			document.getElementById('clickbutton').title = 'Click Me! (+' + (1*this.mouseUpgrades) + ')'
+ 	    this.setNbrOwned(this.nbrOwned - value)
+      main.update()
+  	}
+  }
     
 }
